28e527a46f5edb26331375a0c484ecbfcdd5a43e
Anousone Silavong <silavongsone@gmail.com>
2025-09-12 15:54:45 +0700

    Build static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/portfolio/' // ແກ້ຕາມ repo name ຂອງ GitHub
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
